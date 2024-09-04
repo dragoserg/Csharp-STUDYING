9bc7ec01c1ac3630ab1966dfb7f9780b4fe25c44
Max <itmax2033@gmail.com>
2024-09-04 13:14:44 +0300

    in progress

---
+++ Mnogoagentnoe/Task 2/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Task_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.populationError = new System.Windows.Forms.Label();
+            this.lblTest = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] { this.Column1, this.Column2, this.Column3, this.Column4 });
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(204, 628);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 50;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 50;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 50;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column4";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 50;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(210, 547);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(789, 69);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(227, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Популяция";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(227, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // populationError
+            // 
+            this.populationError.ForeColor = System.Drawing.Color.Red;
+            this.populationError.Location = new System.Drawing.Point(227, 89);
+            this.populationError.Name = "populationError";
+            this.populationError.Size = new System.Drawing.Size(100, 23);
+            this.populationError.TabIndex = 4;
+            // 
+            // lblTest
+            // 
+            this.lblTest.Location = new System.Drawing.Point(227, 407);
+            this.lblTest.Name = "lblTest";
+            this.lblTest.Size = new System.Drawing.Size(239, 42);
+            this.lblTest.TabIndex = 5;
+            this.lblTest.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1011, 628);
+            this.Controls.Add(this.lblTest);
+            this.Controls.Add(this.populationError);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label lblTest;
+
+        private System.Windows.Forms.Label populationError;
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Button btnStart;
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+
+        #endregion
+    }
+}
---
+++ Mnogoagentnoe/Task 2/Form1.cs
@@ -0,0 +1,71 @@
+﻿// ReSharper disable All
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_2
+{
+    public partial class Form1 : Form
+    {
+        private int totalPopulation = 0;
+        private int notSick = 0;
+        private int latency = 0;
+        private int infected = 0;
+        private int recovered = 0;
+
+        private List<DataGridViewCell> notSickList = new List<DataGridViewCell>();
+        private List<DataGridViewCell> latencyList = new List<DataGridViewCell>();
+        private List<DataGridViewCell> infectedList = new List<DataGridViewCell>();
+        private List<DataGridViewCell> recoveredList = new List<DataGridViewCell>();
+        
+        private const int INDEXOFNOTSICK = 0;
+        private const int INDEXOFLATENCY = 1;
+        private const int INDEXOFINFECTED = 2;
+        private const int INDEXOFRECOVERED = 3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void setTable()
+        {
+            dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[INDEXOFNOTSICK].Value = notSick;
+            dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[INDEXOFLATENCY].Value = latency;
+            dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[INDEXOFINFECTED].Value = infected;
+            dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[INDEXOFRECOVERED].Value = recovered;
+        }
+        
+        private void setPopulation()
+        {
+            notSick = totalPopulation - 1;
+            setTable();
+        }
+
+        private void iteration()
+        {
+            
+        }
+
+        private async void btnStart_Click_1(object sender, EventArgs e)
+        {
+            populationError.Text = string.Empty;
+            if (int.TryParse(textBox1.Text, out totalPopulation))
+            {
+                lblTest.Text = totalPopulation.ToString();
+                await Task.Run(() => setPopulation());
+                await Task.Run(() => iteration());
+            }
+            else
+            {
+                populationError.Text = "Неверный ввод";
+                return;
+            }
+        }
+    }
+}
